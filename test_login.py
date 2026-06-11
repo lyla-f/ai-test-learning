bface9f1070bf11cb03167cb88498910a5ba12bb
lyla-f <shiyuan34sd@163.com>
2026-06-11 10:53:04 +0800

    Add files via upload
    封装了一下登录接口

--- test_login.py
+++ test_login.py
@@ -2,27 +2,22 @@ import requests
 import warnings
 warnings.filterwarnings("ignore")
 
-def test_login_success():
+def login(username,password):
 	url = "https://127.0.0.1:7002/api/common/login"
 	data = {
-		"username": "admin",
-		"password":"FsKgVFHIqn5oc2J2eqaxjg=="
+		"username": username,
+		"password": password
 	}
 	response =requests.post(url, json=data, verify=False)
-	result = response.json()
+	return response.json()
 	
+def test_login_success():
+	result = login("admin","FsKgVFHIqn5oc2J2eqaxjg==")
 	assert result["code"] == 200
 	assert result["msg"] =="OK"
-	print("✅ 登录成功，token已获取")
+	print("✅ 登录成功")
 
 def test_login_wrong_password():
-	url ="https://127.0.0.1:7002/api/common/login"
-	data = {
-		"username":"admin",
-		"username":"wrongpassword"
-	}
-	response =requests.post(url,json=data,verify=False)
-	result =response.json()
-	
+	result = login("admin","wrongpassword")
 	assert result["code"] !=200
 	print("✅ 错误密码被正确拦截")
