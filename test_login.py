fc70324543adc99e0e4f337151965798ac94bc99
lyla-f <shiyuan34sd@163.com>
2026-06-11 10:56:32 +0800

    Update test_login.py
    添加了注释

--- test_login.py
+++ test_login.py
@@ -2,22 +2,25 @@ import requests
 import warnings
 warnings.filterwarnings("ignore")
 
-def login(username,password):
-	url = "https://127.0.0.1:7002/api/common/login"
-	data = {
-		"username": username,
-		"password": password
-	}
-	response =requests.post(url, json=data, verify=False)
-	return response.json()
-	
-def test_login_success():
-	result = login("admin","FsKgVFHIqn5oc2J2eqaxjg==")
-	assert result["code"] == 200
-	assert result["msg"] =="OK"
-	print("✅ 登录成功")
+# 封装登录函数，以后所有测试都调用这一个
+def login(username, password):
+    url = "https://127.0.0.1:7002/api/common/login"
+    data = {
+        "username": username,
+        "password": password
+    }
+    response = requests.post(url, json=data, verify=False)
+    return response.json()
 
+# 测试1：正确密码
+def test_login_success():
+    result = login("admin", "FsKgVFHIqn5oc2J2eqaxjg==")
+    assert result["code"] == 200
+    assert result["msg"] == "OK"
+    print("✅ 登录成功")
+
+# 测试2：错误密码
 def test_login_wrong_password():
-	result = login("admin","wrongpassword")
-	assert result["code"] !=200
-	print("✅ 错误密码被正确拦截")
+    result = login("admin", "wrongpassword")
+    assert result["code"] != 200
+    print("✅ 错误密码被拦截")
